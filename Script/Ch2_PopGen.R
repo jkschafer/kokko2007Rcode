e219931338bc6b6e98510d18962ad34f9559d578
Jerred K. Schafer <jkschafer@albany.edu>
2021-04-03 22:03:20 -0400

    add box 2.2

--- Script/Ch2_PopGen.R
+++ Script/Ch2_PopGen.R
@@ -41,5 +41,44 @@ dev.off()
 
 #------------------------------------------------------------------------#
 
+#----------------------------------------------------------------#
+#                   Box 2.2 (pg 27-29)                           #
+#            Corresponds to Fig. 2.4 & 2.5 on pg 30              #
+#----------------------------------------------------------------#
+
+x <- seq(0, 1, by = 0.01)
+
+m <- 5
+y <- m * x/(m * x + 1 -x)
+
+pAA <- x * y
+pAa <- x * (1 - y)
+paA <- (1 - x) * y
+paa <- (1 - x) * (1 - y)
+# Check to see if they sum to 1
+pAA + pAa + paA + paa
+
+N <- 1000
+B <- 20 
+b <- 0.8
+
+nA <- N * (pAA * b * B + 0.5 * pAa * B + 0.5 * paA * b * B)
+na <- N * (0.5 * pAa * B + 0.5 * paA * b * B + paa * B)     
+
+xnew <- nA/(nA + na)
+
+par(mar = c(5, 5, 2, 2))
+plot(1, type = "n", xlab = "Frequency of A\nin generation 0", 
+     ylab = "Frequency of A males\namong mating males", 
+     xlim = c(0, 1), 
+     ylim = c(0, 1))
+
+lines(x, xnew)
+        
+
+        
+        
+        
+
 
 
